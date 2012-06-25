f7a4e04601af9015f9127492eeb590c0d0256af9
Eric J. Holmes <eric@ejholmes.net>
2012-06-25 10:59:07 -0700

    Add --target option to build.

--- lib/catapult/cli.rb
+++ lib/catapult/cli.rb
@@ -14,8 +14,10 @@ module Catapult
 
     desc 'build [asset1 asset2..]', 'Build project'
 
+    method_option :target, :aliases => '-t', :desc => 'Directory to compile assets to'
+
     def build(*assets)
-      target = Pathname('./public/assets')
+      target = Pathname(options[:target] || './public/assets')
 
       say "Building: #{Catapult.root}"
 
